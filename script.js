0a070c6aad20bdcf4fa75f50b3c48b9e6d4749b3
Julian1theshanker <71126399+Julian1theshanker@users.noreply.github.com>
2021-03-11 14:37:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+        nummer1 = document.getElementById("firstNumber").value;
+        nummer2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = nummer1 * nummer2;
+}
+
+function divideBy() 
+{ 
+        nummer1 = document.getElementById("firstNumber").value;
+        nummer2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = nummer1 / nummer2;
+}
